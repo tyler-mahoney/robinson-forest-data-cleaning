1892f92cdd55be54760f7d12b67ef41dfd4bbfe1
tyler-mahoney <tyler.mahoney@louisville.edu>
2022-06-22 11:46:17 -0400

    Wrote percent flowing code

--- tes-script.R
+++ tes-script.R
@@ -2,4 +2,10 @@
 library(ggplot2)
 Fr.data <- read.csv('C:/Users/david/OneDrive/Desktop/EPA/EPA/6 PROJECT 1 KENTUCKY HEADWATER STREAMS/3 DATA/TIME_SERIES/Table_4_FR.csv')
 
+zero.flow.days <- sum(ifelse(na.omit(Fr.data$meandaily_CFS)==0,1,0))
+
+length.record <- length(na.omit(Fr.data$meandaily_CFS))
+
+percent.flowing <- zero.flow.days/length.record*100
+
 ggplot()
