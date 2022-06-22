3f5c71e1c9b39a3c9a1442913f860b5b3cb4cf64
Tyler <tyler.mahoney@louisville.edu>
2022-06-22 11:23:34 -0400

    Read in data, calculate the total number of 0 flow days

---
+++ tes-script.R
@@ -0,0 +1,5 @@
+## Test script to learn git with R Studio 
+library(ggplot2)
+Fr.data <- read.csv('C:/Users/david/OneDrive/Desktop/EPA/EPA/6 PROJECT 1 KENTUCKY HEADWATER STREAMS/3 DATA/TIME_SERIES/Table_4_FR.csv')
+
+ggplot()
